cabbea787c71437c0ad5b105f3452454c53e429a
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2025-04-19 14:30:58 +0300

    create dialogue service

--- backend/dialogue-service/build.gradle.kts
+++ backend/dialogue-service/build.gradle.kts
@@ -18,9 +18,29 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	// Core
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+	// Kafka
+	implementation("org.springframework.kafka:spring-kafka")
+
+	// Utils
+	implementation("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	// Tests
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.testcontainers:mongodb")
 }
 
 tasks.withType<Test> {
