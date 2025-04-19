62e1cf22435d6331747767bcf30062a9a4a41671
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2025-04-19 18:41:50 +0300

    add api gateway structure

---
+++ backend/api-gateway/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// Spring Cloud Gateway
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+
+	// JWT и Security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+	// Circuit Breaker
+	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
+
+	// Логирование
+	implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+
+	// Тесты
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.1")
+	}
+}
+
+tasks.jar {
+	archiveFileName.set("app.jar")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
